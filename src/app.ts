1d58c02b514cde8e3e6f49976b5611f3301e9f83
garchel <paulovictor.co97@gmail.com>
2025-09-24 14:44:36 -0300

    adds log to after patch

--- src/app.ts
+++ src/app.ts
@@ -33,17 +33,10 @@ app.configure(configuration(configurationValidator))
 const ENV = process.env.NODE_ENV || 'development'
 
 const corsOptions = {
-  origin: (origin, callback) => {
-    const whitelist = [
-      'https://colab-telles-empregabilidade-frontend.onrender.com',
-      'https://temp-empregabilidade-frontend.eorpdr.easypanel.host'
-    ];
-    if (!origin || whitelist.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: [
+    'https://colab-telles-empregabilidade-frontend.onrender.com',
+    'https://temp-empregabilidade-frontend.eorpdr.easypanel.host'
+  ],
   credentials: true,
   methods: ['GET','POST','PATCH','PUT','DELETE','OPTIONS'],
   allowedHeaders: ['Content-Type','Authorization']
