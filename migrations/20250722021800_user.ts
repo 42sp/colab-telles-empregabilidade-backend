565451b6d4a1cba4c5450568450fb241e21e56d7
Phil <dev@lipe.ph>
2025-09-26 12:27:38 -0300

    fix: migration

--- migrations/20250722021800_user.ts
+++ migrations/20250722021800_user.ts
@@ -7,6 +7,7 @@ export async function up(knex: Knex): Promise<void> {
 		await knex.schema.createTable('users', table => {
 			table.increments('id')
 			table.string('name').notNullable();
+			table.timestamp('created_at', { useTz: true }).defaultTo(knex.fn.now())
 			table.string('email').unique()
 			table.string('password')
 		})
